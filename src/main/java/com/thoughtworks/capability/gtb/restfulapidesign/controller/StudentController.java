b52f4d85e8daa349973d97a27e573d382fec924d
hongyue-qiu <994059641@qq.com>
2020-11-05 21:49:09 +0800

    add student controller add stu

---
+++ src/main/java/com/thoughtworks/capability/gtb/restfulapidesign/controller/StudentController.java
@@ -0,0 +1,27 @@
+package com.thoughtworks.capability.gtb.restfulapidesign.controller;
+
+import com.thoughtworks.capability.gtb.restfulapidesign.dto.Student;
+import com.thoughtworks.capability.gtb.restfulapidesign.service.StudentService;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class StudentController {
+    private final StudentService studentService;
+
+    public StudentController(StudentService studentService) {
+        this.studentService = studentService;
+    }
+
+    @PostMapping("/students")
+    @ResponseStatus(HttpStatus.CREATED)
+    public List<Student> addStudent(@RequestBody Student student){
+        return studentService.addNewStudent(student);
+    }
+
+}
