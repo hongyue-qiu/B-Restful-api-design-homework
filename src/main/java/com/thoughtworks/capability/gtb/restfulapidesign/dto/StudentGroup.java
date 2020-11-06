8254cfeb40b51d9f502b1558b8711e19d9dee902
hongyue-qiu <994059641@qq.com>
2020-11-06 13:50:13 +0800

    增加分组dto

--- src/main/java/com/thoughtworks/capability/gtb/restfulapidesign/dto/StudentGroup.java
+++ src/main/java/com/thoughtworks/capability/gtb/restfulapidesign/dto/StudentGroup.java
@@ -4,12 +4,12 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class Student {
-    private Integer id;
-    private String name;
-    private String gender;
-    private String note;
+public class StudentGroup {
+    private int id;
+    private List<Student> studentGroupsList;
 }
